c561fa6efb00c34c058ca2574f44e5fccbd931e7
bogismok <bogismok@gmail.com>
2024-10-11 19:20:53 +0200

    Checking out how pushing works.

--- src/main/java/net/bogismok/thedirtystuff/TheDirtyStuff.java
+++ src/main/java/net/bogismok/thedirtystuff/TheDirtyStuff.java
@@ -27,7 +27,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
-
+// gitHub test
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TheDirtyStuff.MOD_ID)
 public class TheDirtyStuff {
